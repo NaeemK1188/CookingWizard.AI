5424ce5a052c2254c243b3e8e7751e85bad0f13b
NaeemK1188 <naeemalbirkdar@aol.com>
2025-03-19 20:53:24 +0000

    testing OpenAI in client side in App()

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,85 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState } from 'react';
 import './App.css';
 
+// title, recipe are from the response from the /api/new-recipe
+// because server.ts at /api/new-recipe endpoint, its sending
+// res.json({ title, recipe: recipeResponse }) an object Recipe
+type Recipe = {
+  title: string;
+  recipe: string;
+};
+
 export default function App() {
-  const [serverData, setServerData] = useState('');
+  // both wil be changing on display and constantly changing, so there states are changing
+  const [requestIngredient, setRequestIngredient] = useState('');
+  const [responseRecipe, setResponseRecipe] = useState<Recipe>(); // we made the responseRecipe as an object Recipe
+  // because the endpoint /api/new-recipe endpoint is sending
+  // res.json({ title, recipe: recipeResponse }) which is an object Recipe or its sending an object
 
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
+  async function handleSubmit() {
+    const request = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ requestIngredient }), // the body we are sending is an object that has an
+      // requestIngredient prop has the value string that are the ingredients types in from the user
+      // or the req.body from the request body from server.ts const { requestIngredient } = req.body;
+    };
 
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
+    const response = await fetch('/api/new-recipe', request);
+    if (!response.ok) {
+      throw new Error(`fetch Error ${response.status}`);
     }
 
-    readServerData();
-  }, []);
+    const recipe = (await response.json()) as Recipe;
+    setResponseRecipe(recipe);
+  }
+  // async function readServerData()
+  // {
+  //   // const resp = await fetch('/api/hello'); testing api from
+  //   // nothing to fetch because no inserted data
+  //   const resp = await fetch('/api/new-recipe');
+  //   const data = await resp.json();
+
+  //   console.log('Data from server:', data);
+
+  //   setServerData(data);
+  // }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* when we made the onChange here, its like letting the
+      to type, But AI is only returning the whole answer all together not typing
+      so we only use the state directly like  {responseRecipe?.recipe} */}
+      <div className="container-response">
+        {/* nothing on screen because server error */}
+        {/* onchange only when user types */}
+        {/* <textarea
+          name="response"
+          cols={70}
+          rows={50}
+          value={responseRecipe}
+          // handles change of input on screen
+          onChange={(event) => setResponseRecipe(event.target.value)}
+        /> */}
+        {responseRecipe?.recipe}
       </div>
-      <h1>{serverData}</h1>
+      {/* the input and textarea are to let the user input
+      we are using onChange because we are letting the user type in*/}
+      <div className="container-request">
+        <textarea
+          name="request"
+          cols={70}
+          rows={6}
+          value={requestIngredient}
+          onChange={(event) => setRequestIngredient(event.target.value)}
+        />
+      </div>
+      <button className="border-width" onClick={handleSubmit}>
+        New Recipe
+      </button>
+      {/* <h1>{serverData}</h1> */}
     </>
   );
 }
